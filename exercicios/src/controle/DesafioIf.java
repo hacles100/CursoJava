8a91d30779727746af907c470e5545ce86eec802
hacles100 <haclesrocha200@gmail.com>
2023-06-16 14:37:38 -0100

    if e else basic done

--- exercicios/src/controle/DesafioIf.java
+++ exercicios/src/controle/DesafioIf.java
@@ -6,6 +6,9 @@ public class DesafioIf {
 		
 		double nota = 1.3;
 		
+		// Não usar ; em estruturas de controle, pois nao teremos 
+		// o resultado esperado
+		
 		if(nota >= 9.0); {
 			System.out.println("Quadro de Honra!");
 			System.out.println("Você é fera!!!");
---
+++ exercicios/src/controle/IfElse.java
@@ -0,0 +1,20 @@
+package controle;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Insira um numero: ");
+		int numero = entrada.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.printf("%d é um numero par! ", numero);
+		}else {
+			System.out.printf("%d é um numero ímpar! ", numero);
+		}
+	}
+}
