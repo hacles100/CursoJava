3e41c5c3026dd6a9dad5f31f8162fd8248e954c4
hacles100 <haclesrocha200@gmail.com>
2023-06-16 16:04:48 -0100

    ciclo for done

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+		
+		for(int contador = 1; contador <= 5; contador++) {
+			System.out.println("contador");
+		}
+	}
+}
